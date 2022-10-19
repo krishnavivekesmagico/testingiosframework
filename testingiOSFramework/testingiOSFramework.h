3bdf6fdcb3acd886c802959e338d9a426256c604
Krishna Vivek <vivek4srirama@yahoo.com>
2022-10-19 20:25:22 +0100

    Initial Commit

---
+++ testingiOSFramework/testingiOSFramework.h
@@ -0,0 +1,18 @@
+//
+//  testingiOSFramework.h
+//  testingiOSFramework
+//
+//  Created by ANSK Vivek on 19/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testingiOSFramework.
+FOUNDATION_EXPORT double testingiOSFrameworkVersionNumber;
+
+//! Project version string for testingiOSFramework.
+FOUNDATION_EXPORT const unsigned char testingiOSFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testingiOSFramework/PublicHeader.h>
+
+
